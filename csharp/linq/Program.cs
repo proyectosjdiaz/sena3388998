b032162146ed309dfea482761cec98fee9533822
Jorge Diaz <proyectos@jorgediaz.com.co>
2026-05-09 08:35:20 -0500

    Add initial implementation of LINQ functionality with product filtering and selection

---
+++ csharp/linq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+ 
+List<Producto> productos = new()
+{
+    new Producto("Teclado", 180000m),
+    new Producto("Mouse", 75000m),
+    new Producto("Monitor", 650000m),
+    new Producto("USB", 25000m),
+};
+ 
+// Filtrar productos "caros"
+var caros = productos.Where(p => p.Precio > 100000m);
+ 
+foreach (var p in caros)
+{
+    p.MostrarInformacion();
+}
+ 
+// Obtener solo los nombres
+var nombres = productos.Select(p => p.Nombre).ToList();
+ 
+// Contar cuántos productos cuestan más de 100.000
+int cantidadCaros = productos.Count(p => p.Precio > 100000m);
